17e2486941b1d8e424bb77853c4fe3f4de9cdd0d
Daniel Rodriguez <danny@dannyrodriguez.com>
2019-10-10 13:12:40 -0400

    Untracked Changes V3 Oct 10 1PM

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
 import { HomepageComponent } from './homepage/homepage.component';
 import {MatCardModule} from "@angular/material/card";
@@ -9,10 +8,12 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatTableModule} from "@angular/material/table";
 import {MatExpansionModule} from "@angular/material/expansion";
+import { TOSComponent } from './homepage/tos/tos.component';
+import { PrivacyComponent } from './homepage/privacy/privacy.component';
 
 
 @NgModule({
-  declarations: [HomepageComponent],
+  declarations: [HomepageComponent, TOSComponent, PrivacyComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
--- client/src/app/nav/navbar/navbar.component.ts
+++ client/src/app/nav/navbar/navbar.component.ts
@@ -1,10 +1,7 @@
-import { MatDialog } from '@angular/material';
 import { CourseNav } from './../../courses/courses.models';
 import { Subscription } from 'rxjs/internal/Subscription';
 import { UserService } from './../../user.service';
 import { Component, OnInit } from '@angular/core';
-import { CourseDetailEditorComponent } from '../../courses/course/info/course-detail-editor/course-detail-editor.component';
-import {NewcourseComponent} from '../newcourse/newcourse.component';
 
 @Component({
   selector: 'app-navbar',
@@ -18,10 +15,7 @@ export class NavbarComponent implements OnInit {
   private student_id = '';
   private subscriptions: Subscription[] = [];
 
-  constructor(
-    private userServices: UserService,
-    private dialog: MatDialog,
-  ) {
+  constructor(private userServices: UserService) {
     this.student_id = this.userServices.user(); // get debug student id
   }
 
@@ -29,22 +23,6 @@ export class NavbarComponent implements OnInit {
   ngOnInit() {
 
     this.loadCourses();
-  }
-
-  openAddCourseDialog() {
-    const dialogRef = this.dialog.open(NewcourseComponent, {
-      width: '90%',
-      data: {
-        name: 'Insert Course Title Here',
-        description: 'Enter Course description here'
-      }
-    });
-
-    this.subscriptions.push(dialogRef.afterClosed().subscribe( (result) => {
-      if(result) {
-        console.log(result);
-      }
-    }));
   }
 
   /**
@@ -64,3 +42,70 @@ export class NavbarComponent implements OnInit {
     return this.userServices.getIsAdmin();
   }
 }
+
+// import { MatDialog } from '@angular/material';
+// import { CourseNav } from './../../courses/courses.models';
+// import { Subscription } from 'rxjs/internal/Subscription';
+// import { UserService } from './../../user.service';
+// import { Component, OnInit } from '@angular/core';
+// import { CourseDetailEditorComponent } from '../../courses/course/info/course-detail-editor/course-detail-editor.component';
+// import {NewcourseComponent} from '../newcourse/newcourse.component';
+//
+// @Component({
+//   selector: 'app-navbar',
+//   templateUrl: './navbar.component.html',
+//   styleUrls: ['./navbar.component.scss']
+// })
+// export class NavbarComponent implements OnInit {
+//
+//   myCourses: CourseNav[] = []; // the user's courses names and id
+//
+//   private student_id = '';
+//   private subscriptions: Subscription[] = [];
+//
+//   constructor(
+//     private userServices: UserService,
+//     private dialog: MatDialog,
+//   ) {
+//     this.student_id = this.userServices.user(); // get debug student id
+//   }
+//
+//   // Runs whenever this component is loaded
+//   ngOnInit() {
+//
+//     this.loadCourses();
+//   }
+//
+//   openAddCourseDialog() {
+//     const dialogRef = this.dialog.open(NewcourseComponent, {
+//       width: '90%',
+//       data: {
+//         name: 'Insert Course Title Here',
+//         description: 'Enter Course description here'
+//       }
+//     });
+//
+//     this.subscriptions.push(dialogRef.afterClosed().subscribe( (result) => {
+//       if(result) {
+//         console.log(result);
+//       }
+//     }));
+//   }
+//
+//   /**
+//    * Load courses, id and name, for the current user.
+//    */
+//   loadCourses() {
+//     this.subscriptions.push(this.userServices.getStudentCourses(this.student_id).subscribe( (resp: CourseNav[]) => {
+//       this.myCourses = resp;
+//     } ));
+//   }
+//
+//   toggleAdmin() {
+//     this.userServices.ToggleIsAdmin();
+//   }
+//
+//   isAdmin() {
+//     return this.userServices.getIsAdmin();
+//   }
+// }
