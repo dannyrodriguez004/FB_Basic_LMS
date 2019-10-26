97e205327098637ad5f07c0740e16bc1142b2f2a
Joao Guimaraes <jguim002@fiu.edu>
2019-10-26 12:15:07 -0400

    basic desing of courses page

--- client/src/app/courses/courses/courses.component.ts
+++ client/src/app/courses/courses/courses.component.ts
@@ -1,9 +1,11 @@
+import { MatDialog } from '@angular/material';
 import { Component, OnInit } from '@angular/core';
 import {CoursesService} from '../courses.service';
 import {UserService} from '../../user.service';
 import {Subscription} from 'rxjs';
 import {Router} from '@angular/router';
 import {Course} from '../courses.models';
+import { YesNoDialogComponent } from 'src/app/yes-no-dialog/yes-no-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -13,28 +15,42 @@ import {Course} from '../courses.models';
 export class CoursesComponent implements OnInit {
 
   private subscriptions: Subscription[] = [];
-  courses: Course[] = []; // the user's courses names and id
-  course = {name: '', id: '', description: '', instructor: ''};
+  courses = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; // the user's courses names and id
+
 
   constructor(
     private userServices: UserService,
     private coursesServices: CoursesService,
-    private router: Router
+    private router: Router,
+    private dialog: MatDialog
   ) {
   }
 
-  loadData() {
-    this.subscriptions.push(this.coursesServices.getAllCourses().subscribe( (resp: Course[]) => {
-      this.courses = resp;
-    }));
+  ngOnInit() {
+    this.fetchPage();
   }
 
-  ngOnInit() {
-    this.loadData();
-    // this.subscriptions.push(this.router.events.subscribe((e:any) => {
-    //   if(e instanceof NavigationEnd) {
-    //     this.loadData();
-    //   }
-    // }));
+
+  fetchPage(){
+  }
+
+  enroll() {
+
+  }
+
+  openFullDialog() {
+    const dialogRef = this.dialog.open(YesNoDialogComponent, {
+      width: "50%",
+      data: {
+        title: "Course is Full!",
+        message: "Would you like to be placed in the waiting list?\n\nYou will be notified if you a spot becomes available!"
+      }
+    });
+
+    dialogRef.afterClosed().subscribe( (resp) => {
+      if(resp) {
+        console.log("trying to insert into class");
+      }
+    })
   }
 }
